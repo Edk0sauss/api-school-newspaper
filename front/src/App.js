8b50e464456d70bcf7f061fc7e4120038b29435c
Damien <damien.armillon@gmail.com>
2019-05-18 11:13:55 +0200

    Add link to admin

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Image} from 'semantic-ui-react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Image, Button} from 'semantic-ui-react'
+import { BrowserRouter as Router, Route,Link } from 'react-router-dom';
 import './App.css';
 import AdminPage from './view/AdminPage/AdminPage';
 import AllToucanPage from './view/AllToucanPage/AllToucanPage';
@@ -14,6 +14,9 @@ function App() {
       <div style={{verticalAlign:"center"}}>
       <Image src={logo} alt="Logo" href="/" style={{marginLeft:'30%', width:'40%'}} />
       <Router >
+        <Link to="/admin" style={{float:"right", margin:"2em"}} >
+          <Button icon="male" content="Espace admin"/>
+        </Link>
         <Route exact path='/' component={AllToucanPage} />
         <Route exact path='/admin' render={()=>isLogged() ? <AdminPage/> : window.location=`${env.backURL}/oauth/login`}/>
         <Route exact path='/login/:token' component={Login} />
